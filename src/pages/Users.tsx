c24915583677791e0b18324d2fb1f666fbd37807
Maxim Ivanov <maximgrozzenberg@yandex.ru>
2023-08-02 23:23:35 +0300

    correct_Users

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -27,191 +27,177 @@ export const Users = () => {
             <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px] "
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
-        <div className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]">
+        <div
+          onClick={() => setModalActive(true)}
+          className="grid grid-cols-2 w-[1304px] h-[60px] shadow-md mb-[15px]"
+        >
           <div className="flex ">
             <img
               src={id_logo}
               alt="id"
-              className="w-[20px] h-[20px]"
+              className="w-[20px] h-[20px] ml-[20px] mt-[20px] mb-[20px]"
             />
-            <span>ФИО</span>
+            <span className="ml-[12px] mt-[15px] mb-[20px]">ФИО</span>
           </div>
           <div className="flex font-Commissioner">
-            <img
-              src={email_logo}
-              alt="Email"
-              className="w-[20px] h-[20px]"
-            />
+            <img src={email_logo} alt="Email" className="w-[20px] h-[20px] " />
             <span>Email</span>
           </div>
         </div>
